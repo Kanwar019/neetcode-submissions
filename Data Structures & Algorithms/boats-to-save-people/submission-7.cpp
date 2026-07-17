de162dd062ca4d7c6917fd32d4fa5ef6c0fd9696
Kanwar <ksingh2_be23@thapar.edu>
2026-07-17 23:54:45 +0530

    Add: boats-to-save-people - submission-7

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-7.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        int left = 0;
+        int right = people.size() - 1;
+        vector<vector<int>> res;
+        sort(people.begin(), people.end());
+        
+        while (left <= right) {
+            if (left == right) {
+                res.push_back({people[left]});
+                break;
+            } 
+            else if (people[left] + people[right] <= limit) {
+                res.push_back({people[left], people[right]});
+                left++;
+                right--;
+            } 
+            else {
+                res.push_back({people[right]});
+                right--;
+            }
+        }
+        return res.size();
+    }
+};
