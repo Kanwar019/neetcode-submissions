7eaa31623470b35efbfb6626b8fe8e4dd95d7bcf
Kanwar <ksingh2_be23@thapar.edu>
2026-09-06 14:06:44 +0530

    Add: house-robber - submission-3

---
+++ Data Structures & Algorithms/house-robber/submission-3.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        if(n==0) return 0;
+        vector<int> t(n+1,0);
+        t[1]=nums[0];
+        for(int i=2;i<=n;i++){
+            int steal=nums[i-1]+t[i-2];
+            int skip=t[i-1];
+            t[i]=max(skip,steal);
+        }
+        return t[n];
+    }
+};
