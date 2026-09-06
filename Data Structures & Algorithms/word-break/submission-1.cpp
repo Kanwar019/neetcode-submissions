0de0b4c57cdd2c8791370518a901896222c429b1
Kanwar <ksingh2_be23@thapar.edu>
2026-09-06 15:33:06 +0530

    Add: word-break - submission-1

---
+++ Data Structures & Algorithms/word-break/submission-1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    unordered_set<string> st;
+    int t[201];
+    int n;
+    bool solve(int idx,string &s){
+          if(idx>=n) return true;
+          if(t[idx]!=-1) return t[idx];
+          for(int l=1;l<=n;l++){
+            string temp=s.substr(idx,l);
+            if(st.find(temp)!=st.end() && solve(idx+l,s)){
+                return t[idx]=true;
+            }
+          }
+          return t[idx]=false;
+    }
+    bool wordBreak(string s, vector<string>& wordDict) {
+        n=s.length();
+        for(int i=0;i<n;i++){
+            t[i]=-1;
+        }
+        for(string &word:wordDict){
+            st.insert(word);
+        }
+        return solve(0,s);
+    }
+};
